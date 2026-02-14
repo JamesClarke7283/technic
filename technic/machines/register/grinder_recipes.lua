5c229490161744daeab7e14eb0847801b098e93c
James David Clarke <james@jamesdavidclarke.com>
2026-02-14 11:51:41 +0000

    Added MCL grinder recipes

--- technic/machines/register/grinder_recipes.lua
+++ technic/machines/register/grinder_recipes.lua
@@ -43,6 +43,7 @@ local dependent_recipes = {
 	moreores = {
 		{"moreores:mithril_lump",	"technic:mithril_dust 2"},
 		{"moreores:silver_lump",	"technic:silver_dust 2"},
+		{"moreores:tin_lump",		"technic:tin_dust 2"},
 	},
 	nether = {
 		{"nether:nether_lump",		"technic:nether_dust 2"},
@@ -57,10 +58,19 @@ for dependency, materials_to_add in pairs(dependent_recipes) do
 	end
 end
 
+-- MCL raw ore lump -> dust recipes
+if technic_compat.mcl then
+	table.insert(recipes, {technic_compat.iron_lump_ingredient, "technic:iron_dust 2"})
+	table.insert(recipes, {technic_compat.gold_lump_ingredient, "technic:gold_dust 2"})
+	table.insert(recipes, {technic_compat.copper_lump_ingredient, "technic:copper_dust 2"})
+end
+
 -- Defuse sandstone -> 4 sand recipe to avoid infinite sand bugs
-minetest.clear_craft({ recipe = {{"default:sandstone"}} })
-minetest.clear_craft({ recipe = {{"default:desert_sandstone"}} })
-minetest.clear_craft({ recipe = {{"default:silver_sandstone"}} })
+if minetest.get_modpath("default") then
+	minetest.clear_craft({ recipe = {{"default:sandstone"}} })
+	minetest.clear_craft({ recipe = {{"default:desert_sandstone"}} })
+	minetest.clear_craft({ recipe = {{"default:silver_sandstone"}} })
+end
 
 if minetest.get_modpath("everness") then
 	minetest.clear_craft({ recipe = {{"everness:mineral_sandstone"}} })
@@ -71,46 +81,59 @@ for _, data in ipairs(recipes) do
 end
 
 -- Dusts
-local function register_dust(name, ingot)
-	local lname = string.lower(name):gsub(' ', '_')
-	minetest.register_craftitem("technic:"..lname.."_dust", {
+local function register_dust(name, ingot, texture_name)
+	local lname = texture_name or string.lower(name):gsub(' ', '_')
+	local item_lname = string.lower(name):gsub(' ', '_')
+	minetest.register_craftitem("technic:"..item_lname.."_dust", {
 		description = S("%s Dust"):format(S(name)),
 		inventory_image = "technic_"..lname.."_dust.png",
 	})
 	if ingot then
 		minetest.register_craft({
 			type = "cooking",
-			recipe = "technic:"..lname.."_dust",
+			recipe = "technic:"..item_lname.."_dust",
 			output = ingot,
 		})
-		technic.register_grinder_recipe({ input = {ingot}, output = "technic:"..lname.."_dust 1" })
+		technic.register_grinder_recipe({ input = {ingot}, output = "technic:"..item_lname.."_dust 1" })
 	end
 end
 
 local dusts = {
 	{"Brass",           "basic_materials:brass_ingot"},
-	{"Bronze",          "default:bronze_ingot"},
+	{"Bronze",          technic_compat.bronze_ingredient},
 	{"Carbon Steel",    "technic:carbon_steel_ingot"},
 	{"Cast Iron",       "technic:cast_iron_ingot"},
 	{"Chernobylite",    "technic:chernobylite_block"},
 	{"Chromium",        "technic:chromium_ingot"},
 	{"Coal",            nil},
-	{"Copper",          technic_compat.copper_ingredient or "default:copper_ingot"},
 	{"Lead",            "technic:lead_ingot"},
-	{"Gold",            technic_compat.gold_ingot_ingredient or "default:gold_ingot"},
-	{"Mithril",         "moreores:mithril_ingot"},
-	{"Silver",          "moreores:silver_ingot"},
 	{"Stainless Steel", "technic:stainless_steel_ingot"},
-	{"Stone",           "default:stone"},
+	{"Stone",           technic_compat.stone_ingredient},
 	{"Sulfur",          nil},
 	{"Tin",             technic_compat.tin_ingredient or "default:tin_ingot"},
 	{"Wrought Iron",    "technic:wrought_iron_ingot"},
 	{"Zinc",            "technic:zinc_ingot"},
 }
 
+-- Add base metal dusts only when their source mod is present
+if technic_compat.mcl then
+	table.insert(dusts, {"Iron",   "mcl_core:iron_ingot", "wrought_iron"})
+	table.insert(dusts, {"Gold",   "mcl_core:gold_ingot"})
+	if minetest.get_modpath("mcl_copper") then
+		table.insert(dusts, {"Copper", "mcl_copper:copper_ingot"})
+	end
+elseif minetest.get_modpath("default") then
+	table.insert(dusts, {"Gold",   "default:gold_ingot"})
+	table.insert(dusts, {"Copper", "default:copper_ingot"})
+end
+
 local dependent_dusts = {
 	everness = {
 		{"Pyrite",          "everness:pyrite_ingot"},
+	},
+	moreores = {
+		{"Mithril",         "moreores:mithril_ingot"},
+		{"Silver",          "moreores:silver_ingot"},
 	},
 	gloopores = {
 		{"Akalin",          "glooptest:akalin_ingot"},
@@ -133,7 +156,7 @@ for dependency, dusts_to_add in pairs(dependent_dusts) do
 end
 
 for _, data in ipairs(dusts) do
-	register_dust(data[1], data[2])
+	register_dust(data[1], data[2], data[3])
 end
 
 -- Uranium Logic remains same...
